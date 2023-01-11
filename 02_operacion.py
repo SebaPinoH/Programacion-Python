b433f235f596980acbed852735b86088c79432c2
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-01-11 00:58:37 -0300

    Create 02_operacion.py

---
+++ 02_operacion.py
@@ -0,0 +1,22 @@
+#Operaciones Basicas
+
+numero_a = 5
+numero_b = 10
+numero_c = 7
+numero_d = 2
+
+print("La suma de los numeros a y b es: ", numero_a + numero_b)
+print("La resta de los numeros c y b es: ", numero_c - numero_b)
+print("La multiplicacion de los numeros a y b es: ", numero_a * numero_b)
+print("La division de los numeros b y a es: ", numero_b / numero_a)#devolvera siempre un valor flotante
+print("La division de los numeros b y a es: ", numero_b // numero_a)#devolvera siempre un valor entero
+print("La division de los numeros b y a es: ", numero_b % numero_a)#devolvera siempre el resto de la division
+print("La division de los numeros b y a es: ", numero_b / numero_a)#devolvera siempre un valor flotante
+print("Calcula la potencia de un numero: ", numero_d ** 3)
+
+
+#Operaciones de Comparar
+print(numero_a > numero_b)
+print(numero_a < numero_b)
+print(numero_a = numero_d)
+
