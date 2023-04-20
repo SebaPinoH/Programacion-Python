19f549854727e38b1149ec45e965354dee9b6892
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-04-20 16:14:57 -0400

    Update Sumar_y_Muestra_Resultado.py

--- Sumar_y_Muestra_Resultado.py
+++ Sumar_y_Muestra_Resultado.py
@@ -1,3 +1,7 @@
+"""     Se utiliza la función input() para solicitar al usuario que ingrese dos números. La función input() devuelve una cadena de texto, por lo que es necesario convertir los valores ingresados a números enteros utilizando la función int().
+    Los números ingresados se suman y se guarda el resultado en la variable resultado.
+    Finalmente, se utiliza la función print() para mostrar el resultado por pantalla. Se utiliza la concatenación de cadenas de texto y la coma para separar el mensaje del valor que se desea mostrar. """
+
 num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 
