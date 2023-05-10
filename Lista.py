c5509a36fe2a5f63bebc0448bd247fca8c8e1107
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-05-09 23:08:00 -0400

    Create Lista.py

---
+++ Lista.py
@@ -0,0 +1,13 @@
+Lista = ['Python',120,'Nombre',4.13,9]
+
+print(type(Lista))
+print(len(Lista))
+print(Lista)
+
+#LLENAR UNA LISTA VACIA
+
+lista1 = []
+
+edad = int(input("Ingresa tu edad: "2))
+lista1.append(edad)
+print(lista1)
