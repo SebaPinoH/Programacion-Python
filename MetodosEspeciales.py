fe8d005c93866b545e1e462bc5101bf78e200b01
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-05-09 23:19:23 -0400

    Create MetodosEspeciales.py

---
+++ MetodosEspeciales.py
@@ -0,0 +1,21 @@
+#el metodo def del, es un metodo destructor cuando finaliza todo el pusle del objecto, se ejecuta el destrucctor y elimina todo lo creado tanto en memoria como en la funcion.
+
+class FabricaTelefono():
+    def __init__(self, marca, color):
+        self.marca = marca
+        self.color = color
+
+        print ("El objecto {} ha sido creado".format(self.marca))
+    
+#clase por defecto de python para poder entender este tipo de mensaje <__main__.FabricaTelefono object at 0x104c84a90>
+    def __str__(self):
+        return "El objecto {}".format(self.marca)
+
+    def __del__(self):
+        print("El objecto{} ha sido destruido".format(self.marca))
+
+
+telefono = FabricaTelefono("Nokia","Negro")
+
+print(telefono.marca)
+print(telefono)
