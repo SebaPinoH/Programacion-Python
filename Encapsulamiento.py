3cd1ed374624e0301f632e5953d5f4167cf0255a
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-05-11 01:31:07 -0400

    Update Encapsulamiento.py

--- Encapsulamiento.py
+++ Encapsulamiento.py
@@ -35,4 +35,29 @@ print(b.cuenta())
 b.incrementar()
 print(b.cuenta())
 #Esto es encapsulamiento por eso es el error, ya que no se puede acceder por fuerea de la clase de donde nacio, es una mala practica en cual era mandar de esta manera los atributos
-print(b.__contador) 
+print(b.__contador)
+
+#RECORDAR
+#Encapsular es solo para los atributos, ahora bien hay ciertas reglas que se deben respertar y seguir para poder lograr un buen encapsulamiento
+#Reutimializamos la clase anterior pero con las variables encapsulada
+#El _ dira que es encapsulado, en cual la mejor manera de llamar es utilizando los metodos de GET y SET, mirar el ejemplo MetoroGET.py
+
+
+class A():
+    def __init__(self):
+        self._contador = 0
+        self._cuenta = 0
+
+    def incrementar(self):
+        self._contador += 1
+
+    def cuenta(self):
+        return self._contador
+
+print("Obejto Encapsulado")
+
+a = A()
+#esta es una manera incorrecta de llamar. utilizar los metodos de GET y SET
+print(a._cuenta())
+a._cuenta = 20
+print(a._cuenta())
