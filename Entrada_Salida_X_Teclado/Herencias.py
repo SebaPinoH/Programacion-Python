d4ab073d31f0e98e94b918afaf04aaeddfdff335
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-06-01 14:57:36 -0400

    Update Herencias.py

--- Entrada_Salida_X_Teclado/Herencias.py
+++ Entrada_Salida_X_Teclado/Herencias.py
@@ -1,3 +1,23 @@
 class Animales():
     def habla(self):
-        print
+        print("Yo soy un animal")
+
+    def descripcion(self):
+        print("Yo soy un {}" .format(self.animal))    
+
+class Perro(Animales):
+    pass
+
+
+class Abeja(Animales):
+    def __init__(self, animal):
+        self.animal = animal
+
+animal = Animales()
+animal.habla()
+
+perro = Perro()
+perro.habla()
+
+abeja = Abeja("MONO")
+abeja.descripcion()
