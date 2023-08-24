e50efc6804abe6e8c37128958e12b08fadaa45ca
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-08-24 00:43:06 -0400

    Create Ejercicio1.py

---
+++ Ejercicios_POO/Ejercicio1.py
@@ -0,0 +1,2 @@
+#Esta es una clase llamada Estudiantem con atributos de nombre y nota
+#Definiremos los metodos de iniciarlizar sus atributos, imprimir, y mostrar mensaje aprobado o no por pantalla.
