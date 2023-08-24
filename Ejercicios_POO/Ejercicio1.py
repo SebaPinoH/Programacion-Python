3bb687ddc20ce67c0ced9519fa679219680fd187
SebaPinoH <sebastian.pinoherrera@gmail.com>
2023-08-24 01:41:50 -0400

    Update Ejercicio1.py

--- Ejercicios_POO/Ejercicio1.py
+++ Ejercicios_POO/Ejercicio1.py
@@ -1,2 +1,20 @@
 #Esta es una clase llamada Estudiantem con atributos de nombre y nota
 #Definiremos los metodos de iniciarlizar sus atributos, imprimir, y mostrar mensaje aprobado o no por pantalla.
+
+class Estudiante():
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def imprimir(self):
+        print("Nombre: {} \n Nota: {}".format(self.nombre, self.nota))
+
+    def resultado(self):
+        if self.nota < 7:
+            print("Has Reprobado!")
+        else:
+            print("Has Aprobado!")
+
+estudiante1 = Estudiante('Sebastian', 10)
+estudiante1.imprimir()
+estudiante1.resultado()
